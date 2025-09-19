ec6f137573d3a2e1013999f13b8b1772d010219b
rjrbio <joseftf1991@gmail.com>
2025-09-19 19:22:33 +0200

    commit1

--- src/conexion.php
+++ src/conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = mysqli_connect("db", "root", "test", "Banca");
+$conexion = mysqli_connect("mysql-rjrbio.alwaysdata.net", "rjrbio", "Killoloko00", "rjrbio_banco");
 if (!$conexion) {
     die("Error al conectar con la base de datos");
 }
