78a3939ffac45cb41e3d2d72f5ed899ac67e2d48
rjrbio <joseftf1991@gmail.com>
2025-09-19 20:07:44 +0200

    ocultardatos

--- src/conexion.php
+++ src/conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = mysqli_connect("mysql-rjrbio.alwaysdata.net", "rjrbio", "Killoloko00", "rjrbio_banco");
+$conexion = mysqli_connect("db", "user", "pass", "banco");
 if (!$conexion) {
     die("Error al conectar con la base de datos");
 }
